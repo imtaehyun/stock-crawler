a9411c91982e7bf10eec967435befb8c08bfcf86
nezz <allstars@gmail.com>
2017-08-22 01:28:58 +0900

    web 요소 추가

---
+++ web.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="./static/dist", template_folder="./templates")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
