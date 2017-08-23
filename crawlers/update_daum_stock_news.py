680bdc54df2df6e47200ecc2a575ab43861f5dc2
nezz <imtaehyun@me.com>
2017-08-23 18:14:28 +0900

    daum news 파싱 로직 수정

--- crawlers/update_daum_stock_news.py
+++ crawlers/update_daum_stock_news.py
@@ -1,12 +1,12 @@
+import time
+from datetime import datetime, timedelta
 from urllib.parse import urlparse, parse_qs
 
 import requests
-import time
 from scrapy.selector import Selector
-from sqlalchemy import func
 
 from database import session
-from models import StockNews
+from models import StockNews, SEOUL_TZ
 from utils.logger import Logger
 from utils.slack import Slack
 
@@ -14,13 +14,12 @@ logger = Logger().get_logger()
 slack = Slack()
 
 
-def get_news_list(last_id=None, limit=30, page=1):
+def get_news_list(limit=30, page=1):
     """다음 금융 뉴스 리스트 크롤링"""
     r = requests.get('http://finance.daum.net/news/news_list.daum?type=all&section=&limit={}&page={}'.format(limit, page))
 
     if r.status_code == 200:
         news_list = list()
-        new_last_id = None
 
         for idx, news_selector in enumerate(Selector(text=r.text).css('.newsList li')):
             link = 'http://finance.daum.net' + news_selector.css('a::attr(href)').extract()[0]
@@ -29,15 +28,9 @@ def get_news_list(last_id=None, limit=30, page=1):
             offerer = news_selector.css('span.offerer::text').extract()[0]
             date = news_selector.css('span.datetime::text').extract()[0]
 
-            if idx == 0:
-                new_last_id = doc_id
-
-            if last_id and last_id == doc_id:
-                break
-
             news_list.append(dict(link=link, doc_id=doc_id, title=title, offerer=offerer, date=date))
 
-        return news_list, new_last_id
+        return news_list
     else:
         raise Exception(r.status_code)
 
@@ -58,26 +51,28 @@ def update_daum_stock_news():
     try:
         start_time = time.time()
 
-        last_id = session.query(func.max(StockNews.id)).scalar()
-        logger.info('last_id: {}'.format(last_id))
+        db_news = [news[0] for news in session.query(StockNews.id).filter(StockNews.date >= datetime.now(SEOUL_TZ) - timedelta(minutes=10)).all()]
 
-        news_list, new_last_id = get_news_list(last_id=last_id, limit=50)
-        for news in news_list:
-            session.add(StockNews(id=news.get('doc_id'), offerer=news.get('offerer'), title=news.get('title'), link=news.get('link'), date=news.get('date')))
+        affected_rows = 0
+        news_list = get_news_list(limit=50)
+        for news in (x for x in news_list if x.get('doc_id') not in db_news):
+            date = datetime.strptime(news.get('date'), '%y.%m.%d %H:%M')
+            session.add(StockNews(id=news.get('doc_id'), offerer=news.get('offerer'), title=news.get('title'), link=news.get('link'), date=date))
+            affected_rows += 1
 
-        if len(news_list) > 0:
+        if affected_rows > 0:
             session.commit()
 
         execution_time = time.time() - start_time
 
         logger.info('execution_time: {}'.format(execution_time))
-        logger.info('{} rows added'.format(len(news_list)))
+        logger.info('{} rows added'.format(affected_rows))
 
     except Exception as e:
-        logger.exception(e, exc_info=True)
+        logger.exception(exc_info=True)
         slack.send_message('BATCH:update_daum_stock_news fail {}'.format(e))
 
 
 if __name__ == '__main__':
-    # update_daum_stock_news()
-    get_news_content('MD20170817230056038')
+    update_daum_stock_news()
+    # get_news_content('MD20170817230056038')
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
+import pytz
 from sqlalchemy import Column, DateTime, String, Integer
 
 from database import Base
-
+SEOUL_TZ = pytz.timezone('Asia/Seoul')
 
 class 뉴스_마스터(Base):
     __tablename__ = '뉴스_마스터'
@@ -14,7 +15,7 @@ class 뉴스_마스터(Base):
     rss_url = Column(String(50))
     etag = Column(String(50))
     modified = Column(String(50))
-    created_at = Column(DateTime, default=datetime.now())
+    created_at = Column(DateTime, default=datetime.now(SEOUL_TZ))
     modified_at = Column(DateTime)
 
     def __init__(self, name, rss_url):
@@ -29,8 +30,9 @@ class StockNews(Base):
     offerer = Column(String(30))
     title = Column(String(50))
     link = Column(String(150))
-    date = Column(String(20))
-    created_at = Column(DateTime, default=datetime.now())
+    date = Column(DateTime)
+    created_at = Column(DateTime, default=datetime.now(SEOUL_TZ))
+    modified_at = Column(DateTime)
 
     def __init__(self, id, offerer, title, link, date):
         self.id = id
@@ -47,7 +49,7 @@ class 종목_마스터(Base):
     시장구분 = Column(String(6))
     종목이름 = Column(String(50))
     거래소코드 = Column(String(12))
-    created_at = Column(DateTime, default=datetime.now())
+    created_at = Column(DateTime, default=datetime.now(SEOUL_TZ))
     modified_at = Column(DateTime)
 
     def __init__(self, 시장구분, 종목코드, 종목이름, 거래소코드):
